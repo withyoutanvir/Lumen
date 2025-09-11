61d1c702ffb789af39a9728c1bfd655f76177bf5
Tanvir <tanvirxng01@gmail.com>
2025-09-11 22:54:41 +0530

    Changes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import userRoutes from './routes/User.routes.js';
-
+import transactionRoutes from './routes/transactionRoutes.js';
 const app = express();
 
 app.use(express.json());
 app.use('/', userRoutes);
-
+app.use('/api/transactions', transactionRoutes);
 app.get('/', (req, res) => {
   res.send('API is running');
 });
