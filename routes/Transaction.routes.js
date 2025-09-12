34d13e6e55d9e1fa4f738f777aa22a57ad61d731
Tanvir <tanvirxng01@gmail.com>
2025-09-12 22:58:57 +0530

    Changes

--- routes/Transaction.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createTransaction, getTransactions } from '../controllers/transaction.controller.js';
-
-const router = Router();
-
-router.post('/', createTransaction);
-router.get('/', getTransactions);
-
-export default router;
--- routes/mail.router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import MailSender from "../controllers/mail.controller.js";
-
-const router = express.Router();
-
-// POST /send-mail
-router.post("/send-mail", MailSender);
-
-export default router;
