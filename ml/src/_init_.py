12e18ccb9dd55a16e7de19a556b9e395e69fed7f
SaiJanila <saijanila@gmail.com>
2025-09-13 15:05:51 +0530

    Delete ml directory

--- ml/src/_init_.py
+++
@@ -1,5 +0,0 @@
-# ml/src/__init__.py
-from .preprocessing import load_data, preprocess_data
-from .churn_prediction import train_churn_model, predict_churn
-from .recommend import train_recommendation_model, recommend_plan
-from .visualize import plot_churn_distribution, plot_feature_importance
